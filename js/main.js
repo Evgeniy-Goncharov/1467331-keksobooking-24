270d5b6861c0fe8d019b8787691e326831db273c
siverskiy <siverskiy00@mail.ru>
2021-09-26 20:39:50 +0300

    Исправляет замечания после проверки домашнего задания

--- js/main.js
+++ js/main.js
@@ -1,29 +1,34 @@
-// Функция возвращает случайное целое число из диапазона, включая значения.
-
-function getRandomInt(min, max) {
-  if (min >= 0 && min < max) {
-    min = Math.ceil(min);
-    max = Math.floor(max);
-    const randomInt = Math.round(Math.random() * (max - min)) + min;
-    return randomInt;
-  }
-
-  return 'Функции передан неверный диапазон.';
+function getRandomNumber (min, max) {
+  return (Math.random() * (max - min)) + min;
 }
 
-// Функция возвращает случайное число из диапазона c требуемым количеством знаков после запятой включая значения.
-
-function getRandomNumber(min, max, symbols) {
-  if (min >= 0 && min < max && symbols >= 0) {
-    symbols = Math.round(symbols);
-    let randomNumber = (Math.random() * (max - min)) + min; //Получаем случайное число
-    randomNumber = Math.trunc(randomNumber * Math.pow(10, symbols)) / Math.pow(10, symbols); //Оставляем нужное количество знаков после запятой
-    return randomNumber;
+function checkRange (min, max) {
+  if (min >= 0 && min < max) {
+    return true;
   }
 
-  return 'Функции передан неверный диапазон.';
+  return false;
+}
+
+function getRandomInt(min, max) {
+  if (checkRange(min, max)) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.round(getRandomNumber(max, min));
+  }
+
+  return new Error('Функции передан неверный диапазон.');
+}
+
+function getRandomFloat(min, max, symbols) {
+  if (checkRange(min, max) && symbols >= 0) {
+    symbols = Math.round(symbols);
+    return Number(getRandomNumber(min, max).toFixed(symbols));
+  }
+
+  return new Error('Функции передан неверный диапазон.');
 }
 
 getRandomInt(0, 10);
 
-getRandomNumber(0, 10, 4);
+getRandomFloat(0, 10, 4);
